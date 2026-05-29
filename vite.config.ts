8fa4608a687162f4a627955768b576ed904bd028
kabzsingh <kabirsingh@ymail.com>
2026-05-29 15:53:06 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
+    routesDirectory: "./src/routes",
+    generatedRouteTree: "./src/routeTree.gen.ts",
   },
 });
